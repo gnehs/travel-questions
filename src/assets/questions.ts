953275f00e959feed397668849e49286a72011a2
gnehs <jayuiop@gmail.com>
2023-09-10 20:51:30 +0800

    refactor questions

---
+++ src/assets/questions.ts
@@ -0,0 +1,164 @@
+export default [
+  ...[
+    {
+      icon: "bx bx-map-alt",
+      question: "全程都由你來看地圖並指路，可以嗎？",
+    },
+    {
+      icon: "bx bx-shower",
+      question: "晚上洗完澡後願意再出門嗎？",
+    },
+    {
+      icon: "bx bx-briefcase",
+      question: "遊玩期間可以工作嗎？",
+    },
+    {
+      icon: "bx bx-dollar",
+      question: "可以接受一次超出預算的臨時行程嗎？",
+    },
+    {
+      icon: "bx bx-taxi",
+      question: "距離目的地超過一公里，願意坐計程車嗎？",
+    },
+    {
+      icon: "bx bx-list-ul",
+      question: "可以接受一些原則，比如要看完動物園每一種動物嗎？",
+    },
+    {
+      icon: "bx bx-time",
+      question: "超過中午12點才起床，可以嗎？",
+    },
+    {
+      icon: "bx bx-book",
+      question: "全程由你一個人做攻略可以嗎？",
+    },
+    {
+      icon: "bx bx-map-pin",
+      question: "臨時不想去某個景點可以嗎？",
+    },
+    {
+      icon: "bx bxs-hot",
+      question: "針對行程有意見分歧時可以接受暫時各玩各的嗎？",
+    },
+    {
+      icon: "bx bx-time",
+      question: "能不能臨時更改會合或出門時間？",
+    },
+    {
+      icon: "bx bxs-baby-carriage",
+      question: "逛街或遊玩途中能接受體力不好的人喊累並頻繁休息嗎？",
+    },
+  ].map((item) => ({ ...item, theme: "出門玩篇" })),
+  ...[
+    {
+      icon: "bx bx-camera",
+      question: "合照發社交平台需要經過對方同意嗎？",
+    },
+    {
+      icon: "bx bx-meh",
+      question: "玩得不開心可以將情緒表露在臉上嗎？",
+    },
+    {
+      icon: "bx bx-camera",
+      question: "可以接受朋友發照片時套上濾鏡或是修圖修很兇嗎？",
+    },
+    {
+      icon: "bx bx-camera",
+      question: "在對方喜歡的景點願意花超過 15 分鐘拍照直到滿意嗎？",
+    },
+    {
+      icon: "bx bx-dollar",
+      question: "每一筆消費希望是可以平分到乾乾淨淨嗎？（1 元或 2 元小零頭）",
+    },
+    {
+      icon: "bx bx-time",
+      question: "可以接受在景點或餐廳排隊超過 30 分鐘嗎？",
+    },
+  ].map((item) => ({ ...item, theme: "體驗感篇" })),
+  ...[
+    {
+      icon: "bx bx-phone",
+      question: "在同一個密閉室內空間時，手機是否需要靜音？",
+    },
+    {
+      icon: "bx bx-moon",
+      question: "半夜醒來上洗手間，可以開燈嗎？",
+    },
+    {
+      icon: "bx bx-bulb",
+      question: "睡覺的時候是否一點光都不能有？",
+    },
+    {
+      icon: "bx bx-bed",
+      question: "你自己會打呼嗎？",
+    },
+    {
+      icon: "bx bx-bed",
+      question: "可以接受對方打呼嗎？",
+    },
+    {
+      icon: "bx bx-bed",
+      question: "偶爾想賴在飯店或民宿一整天，可以接受嗎？",
+    },
+    {
+      icon: "bx bx-phone",
+      question: "能接受旅伴睡前跟伴侶或家人打電話超過兩次嗎？",
+    },
+    {
+      icon: "bx bx-home",
+      question: "是否有住宿品質要求？比如不住青旅或民宿，或價格等要求？",
+    },
+    {
+      icon: "bx bx-bed",
+      question: "同睡一張床的時候能不能接受沒洗澡就躺上去？",
+    },
+    {
+      icon: "bx bx-briefcase",
+      question: "對方的東西可以放你行李箱嗎？",
+    },
+    {
+      icon: "bx bxs-backpack",
+      question:
+        "能接受對方因為懶得背包而把小東西放你的包包裡嗎？（比如行動充）",
+    },
+    {
+      icon: "bx bx-shower",
+      question: "洗澡的時候如果對方或自己穿錯拖鞋可以接受嗎？",
+    },
+    {
+      icon: "bx bx-shower",
+      question: "盥洗用品、化妝品、保養品等可以與對方共用嗎？",
+    },
+    {
+      icon: "bx bx-water",
+      question: "盥洗時洗手台如果弄濕了，需要擦乾嗎？",
+    },
+  ].map((item) => ({ ...item, theme: "空間共享篇" })),
+  ...[
+    {
+      icon: "bx bx-pointer",
+      question: "出門旅遊時能接受一天點一次外送嗎？",
+    },
+    {
+      icon: "bx bx-baguette",
+      question:
+        "點餐時有自己不能退讓的飲食癖好嗎？（比如不吃或不敢吃某樣東西）",
+    },
+    {
+      icon: "bx bx-meh-blank",
+      question: "對方或自己喜歡的美食，如果另一人不喜歡可以直說嗎？",
+    },
+    {
+      icon: "bx bx-happy-heart-eyes",
+      question: "很喜歡的餐廳會在一次旅途中連續去兩天嗎？",
+    },
+    {
+      icon: "bx bx-food-menu",
+      question: "什麽都想吃，但總是吃一口就不吃，可以嗎？",
+    },
+    {
+      icon: "bx bx-camera",
+      question: "可以接受手機先吃嗎？",
+    },
+  ].map((item) => ({ ...item, theme: "吃得快樂篇" })),
+];
